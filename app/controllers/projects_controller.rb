b6eb9ec23d49c8d32764f19575b632b59b05a2ab
“angela-toews“ <angela@arclabsonline.comssh-keygen -t rsa -C angela@arclabsonline.com>
2016-05-30 15:14:26 -0500

    Create a new project-feature complete

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,17 +2,29 @@ class ProjectsController < ApplicationController
   def index
   end
 
-  def new
+  def new 
     @project = Project.new
+  
   end
 
   def create
-  @project = Project.new(project_params)
+    @project = Project.new(project_params)
 
-  if @project.save
-    flash[:notice] = "Project has been created."
-    redirect_to @project
-  else
-    # nothing, yet
+    if @project.save
+      flash[:notice] = "Project has been created."
+      redirect_to @project
+    else
+      # nothing, yet
+    end
   end
+
+  def show
+    @project = Project.find(params[:id])
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:name, :description)
+  end  
 end
--- db/migrate/20160529164159_create_projects.rb
+++ db/migrate/20160529164159_create_projects.rb
@@ -1,5 +1,5 @@
 class CreateProjects < ActiveRecord::Migration
-  def change
+  def up
     create_table :projects do |t|
       t.string :name
       t.string :description
